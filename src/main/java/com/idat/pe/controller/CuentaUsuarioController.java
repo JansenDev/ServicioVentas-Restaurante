bb4a8bebe40b7507a72a35f61e087e7f1956d111
Janssen <seguragjj25@gmail.com>
2020-10-28 19:31:53 -0500

    Cuenta Usuario Implemented

--- src/main/java/com/idat/pe/controller/CuentaUsuarioController.java
+++ src/main/java/com/idat/pe/controller/CuentaUsuarioController.java
@@ -16,13 +16,13 @@ import com.idat.pe.service.ICuentaUsuarioService;
 import com.idat.pe.utils.Messages_Utils;
 
 @RestController
-@RequestMapping("/CuentaUsuario")
+@RequestMapping("/usuario")
 public class CuentaUsuarioController {
 
 	@Autowired
 	ICuentaUsuarioService dao;
 	Messages_Utils plantilla = new Messages_Utils();
-	String msg = "";
+	String mensaje = "";
 
 	@GetMapping
 	public ResponseEntity<List<CuentaUsuario>> findAll() {
@@ -30,41 +30,73 @@ public class CuentaUsuarioController {
 	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<CuentaUsuario> findById(@PathVariable("id") int id) {
+	public ResponseEntity<CuentaUsuario> findById(@PathVariable("id") Integer id) {
 
 		CuentaUsuario CuentaEncontrada = dao.findById(id);
-		msg = plantilla.getNotFound();
+		mensaje = plantilla.getNotFound();
 
 		if (CuentaEncontrada.getId_usuario() == null || CuentaEncontrada.getId_usuario() < 0) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, msg);
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, mensaje);
 		}
 
 		return new ResponseEntity<CuentaUsuario>(dao.findById(id), HttpStatus.OK);
 	}
-	
-	public ResponseEntity<CuentaUsuario> save(CuentaUsuario cuentaUsuario){
+
+	public ResponseEntity<CuentaUsuario> save(CuentaUsuario cuentaUsuario) {
+
+		boolean existeCuenta = dao.exists(	cuentaUsuario.getId_usuario());
+
+		if (cuentaUsuario.getNombre() == null || cuentaUsuario.getContrasena() == null
+				|| cuentaUsuario.getCod_empleado() == null) {
+			mensaje = plantilla.getParamInvalid();
+			throw new ResponseStatusException(HttpStatus.CONFLICT, mensaje);
+		}
+		
+		if( existeCuenta	) {
+			mensaje = plantilla.getParamInvalid();
+			throw new ResponseStatusException(HttpStatus.CONFLICT, mensaje);
+		}
 		
 		CuentaUsuario usuarioCreado = dao.save(cuentaUsuario);
+		return new ResponseEntity<CuentaUsuario>(usuarioCreado, HttpStatus.OK);
+
+	}
+	
+	public ResponseEntity<CuentaUsuario> update(CuentaUsuario cuentaUsuario) {
+
+		boolean existeCuenta = dao.exists(	cuentaUsuario.getId_usuario());
+
+		if (cuentaUsuario.getNombre() == null || cuentaUsuario.getContrasena() == null
+				|| cuentaUsuario.getCod_empleado() == null) {
+			mensaje = plantilla.getParamInvalid();
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, mensaje);
+		}
+		if(  !existeCuenta	) {
+			mensaje = plantilla.getNotExists( cuentaUsuario.getId_usuario() );
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, mensaje);
+		}
 		
-//		if() {
-//			
-//		}
-		
-		return new ResponseEntity<CuentaUsuario>(usuarioCreado,HttpStatus.OK);
-		
+		CuentaUsuario usuarioActualizado = dao.save(cuentaUsuario);
+		return new ResponseEntity<CuentaUsuario>(usuarioActualizado, HttpStatus.OK);
+
 	}
 	
 	
+	public ResponseEntity<Object> delete(Integer id){
+		
+		try {
+			dao.delete(id);
+			mensaje = plantilla.getSuccessDelete(id);
+			return new ResponseEntity<Object>(mensaje , HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Object>(plantilla.getErrorDelete(), HttpStatus.CONFLICT);
+			
+		}
+		
+		
+		
+		
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
